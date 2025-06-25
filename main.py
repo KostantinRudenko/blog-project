34917d6832e9b85494bad27cace94e57265b2568
KostantinRudenko <rudenkokostya10@gmail.com>
2025-06-25 23:03:54 +0300

    added venv and first html template

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def main():
-    return {"status": 200}
+@app.get("/", response_class=HTMLResponse)
+async def main(requests: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": requests}
+    )
